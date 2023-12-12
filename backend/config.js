c1574ebde150f1292aa2113b2b34a1a55822b092
andrecito2001 <oviedojonathan2001@gmail.com>
2023-12-12 09:14:15 -0500

    dddd

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,6 @@
-const FRONTEND_URL =  process.env.FRONTEND_URL || "http://localhost:3000/"
-
-const PORT = process.env.PORT || 3000
+// En tu archivo config.js
+module.exports = {
+    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000/",
+    PORT: process.env.PORT || 3000 // Corrección: elimina el "PORT =" de esta línea
+  };
+  
